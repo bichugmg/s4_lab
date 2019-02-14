e230b3948b319676ea09241b7b82d0047feccc4b
Selin <selinmarvan9@gmail.com>
2019-02-14 12:56:34 +0530

    count file word

--- SD LAB/file_count.sh
+++ SD LAB/file_count.sh
@@ -1,16 +1,50 @@
 line_c=0
-char_c=0
+char_C_c=0
+char_S_c=0
+char_N_c=0
+char_SP_c=0
 alpha_c=0
+word_cont=0
 while read p; do
   for (( i=0; i<${#p}; i++ )); do
 	  if [[ "${p:$i:1}" = [a-z] ]]
 	  then
-		alpha_c=$((alpha_c+1))
+		if [[ "${p:$i:1}" != [A-Z] ]]
+		  then
+			char_S_c=$((char_S_c+1))
+		  fi
 	  fi
+	if [[ "${p:$i:1}" = [A-Z] ]]
+	  then
+		char_C_c=$((char_C_c+1))
+	  fi
+	if [[ "${p:$i:1}" = [0-9] ]]
+	  then
+		char_N_c=$((char_N_c+1))
+	  fi
+	if [[ "${p:$i:1}" = *['!'@#\$%^\&*()_+.,';'' ']* ]]
+	  then
+		char_SP_c=$((char_SP_c+1))
+	  fi
+
+	
 	   char_c=$((char_c+1))
   done
+
+while IFS=';' read -ra ADDR; do
+	      for i in "${ADDR[@]}"; do
+		  word_cont=$((word_cont+1))
+			echo $word_cont
+	      done
+	 done <<< "${p:$i:1}"
+
  line_c=$((line_c+1))
 done <t
 echo "line count $line_c"
-echo "char count $char_c"
-echo "alpha count $alpha_c"
+echo "char Small count $char_S_c"
+echo "char Capital count $char_C_c"
+echo "char numarical count $char_N_c"
+echo "char Special count $char_SP_c"
+echo "char Word count $word_cont"
+
+echo "alpha count $char_c"
