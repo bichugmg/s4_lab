76d2de4e127177c9b51ee90eb24f753ac017dae7
Selin <selinmarvan9@gmail.com>
2019-02-21 12:58:34 +0530

    standard deviation partially complete

--- SD LAB/student_mark.sh
+++ SD LAB/student_mark.sh
@@ -32,14 +32,32 @@ echo $gtotal
 #to find grade
 while IFS="," read -r roll admno roll sub1 sub2 sub3 sub4 sub5 sub6 sub7 total mean; do 
 
-ga=$((meana+2*sd))
-gb=$((meana+sd))
-gc=$((meana))
-gd=$((meana+2*sd))
-ge=$((meana+2*sd))
+ga=$(bc <<< "scale=2; $meana+2*$sd")
+gb=$(bc <<< "scale=2; $meana+$sd")
+gc=$(bc <<< "scale=2; $meana")
+gd=$(bc <<< "scale=2; $meana-$sd")
+ge=$(bc <<< "scale=2; $meana-2*$sd")
 
+if [[ ${total%%.*} -gt ${ga%%.*} ]]
+then
+echo "A"
+elif [[ ${total%%.*} -gt ${gb%%.*} || ${total%%.*} -lt ${ga%%.*} ]]
+then
+echo "B"
+elif [[ ${total%%.*} -gt ${gc%%.*} || ${total%%.*} -lt ${gb%%.*} ]]
+then
+echo "C"
+elif [[ ${total%%.*} -gt ${gd%%.*} || ${total%%.*} -lt ${gc%%.*} ]]
+then
+echo "D"
+elif [[ ${total%%.*} -gt ${ge%%.*} || ${total%%.*} -lt ${gd%%.*} ]]
+then
+echo "E"
+elif [[ ${total%%.*} -lt ${ge%%.*} ]]
+then
+echo "F"
+fi
 
-if [[  ]]
 	
 done < t.csv 
 
